3fae1a96a9bfe0f9384ce6d36c9361e17e2d6967
hutcho <hutcho@users.noreply.github.com>
2024-08-12 21:51:46 +1000

    Cleanup settings page

--- src/model/settings.lua
+++ src/model/settings.lua
@@ -1,167 +1,153 @@
 --------------------------------------
 -- Imports
 --------------------------------------
----@class TodoAddon
-local TodoAddon = select(2, ...)
-
----@class TableUtils
-local TableUtils = TodoAddon.TableUtils
+local _, TodoAddon = ...
 
 --------------------------------------
 -- Declarations
 --------------------------------------
 TodoAddon.Settings = {}
-
----@class Settings
-local Settings = TodoAddon.Settings
+local TCSettings = TodoAddon.Settings
 
 --------------------------------------
 -- Settings functions
 --------------------------------------
 
----@return boolean @Whether or not the window is displayed on the user's screen
-function Settings:IsShown()
+---@return boolean @Get: Window is displayed on the user's screen
+function TCSettings:IsShown()
 	return TodoChecklisterSettingsDB.isShown
 end
 
----@param isShown boolean @Sets whether or not the window is displayed on the user's screen
-function Settings:SetIsShown(isShown)
-	TodoChecklisterSettingsDB.isShown = isShown
+---@param value boolean @Set window is displayed on the user's screen
+function TCSettings:SetIsShown(value)
+	TodoChecklisterSettingsDB.isShown = value
 end
 
----@return boolean @Whether or not the KeepFocus checkbox is checked
-function Settings:KeepFocus()
+---@return boolean @Get: KeepFocus checkbox is checked
+function TCSettings:KeepFocus()
 	return TodoChecklisterSettingsDB.keepFocus
 end
 
----
 ---Toggles whether or not the KeepFocus checkbox is checked
-function Settings:ToggleFocus()
+function TCSettings:ToggleFocus()
 	TodoChecklisterSettingsDB.keepFocus = not self:KeepFocus()
 end
 
----@return boolean @Whether or not the KeepFocus checkbox is displayed to the user
-function Settings:IsKeepFocusShown()
+---@return boolean @Get: KeepFocus checkbox is displayed to the user
+function TCSettings:IsKeepFocusShown()
 	return TodoChecklisterSettingsDB.isKeepFocusShown
 end
 
----@param isKeepFocusShown boolean @Sets whether or not the KeepFocus checkbox is displayed to the user
-function Settings:SetIsKeepFocusShown(isKeepFocusShown)
-	TodoChecklisterSettingsDB.isKeepFocusShown = isKeepFocusShown
+---@param value boolean @Sets whether or not the KeepFocus checkbox is displayed to the user
+function TCSettings:SetIsKeepFocusShown(value)
+	TodoChecklisterSettingsDB.isKeepFocusShown = value
 end
 
----@return number @The alpha value of 0 to 1 for the window opacity
-function Settings:Opacity()
+---@return number @Get the alpha value of 0 to 1 for the window opacity
+function TCSettings:Opacity()
 	return TodoChecklisterSettingsDB.windowOpacity
 end
 
 ---@param opacity number @Sets the alpha value of 0 to 1 for the window opacity
-function Settings:SetOpacity(opacity)
+function TCSettings:SetOpacity(opacity)
 	TodoChecklisterSettingsDB.windowOpacity = opacity
 end
 
----@return boolean @Whether or not a fanfare sound should be played
-function Settings:PlayFanfare()
+---@return boolean @Get if a fanfare sound should be played
+function TCSettings:PlayFanfare()
 	return TodoChecklisterSettingsDB.playFanfare
 end
 
----@param playFanfare boolean @Sets whether or not a fanfare sound should be played
-function Settings:SetPlayFanfare(playFanfare)
-	TodoChecklisterSettingsDB.playFanfare = playFanfare
+---@param value boolean @Sets whether or not a fanfare sound should be played
+function TCSettings:SetPlayFanfare(value)
+	TodoChecklisterSettingsDB.playFanfare = value
 end
 
----@return boolean @Whether or not to display linked items count from bag
-function Settings:DisplayLinked()
+---@return boolean @Get if should display linked items count from bag
+function TCSettings:DisplayLinked()
 	return TodoChecklisterSettingsDB.displayLinked
 end
 
----@param displayLinked boolean @Sets whether or not to display linked items count from bag
-function Settings:SetDisplayLinked(displayLinked)
-	TodoChecklisterSettingsDB.displayLinked = displayLinked
+---@param value boolean @Sets whether or not to display linked items count from bag
+function TCSettings:SetDisplayLinked(value)
+	TodoChecklisterSettingsDB.displayLinked = value
 end
 
 ---@return boolean @Whether or not to display the icon on minimap
-function Settings:DisplayMinimapIcon()
+function TCSettings:DisplayMinimapIcon()
 	return TodoChecklisterSettingsDB.displayMinimapIcon
 end
 
----@param displayMinimapIcon boolean @Whether or not to display the icon on minimap
-function Settings:SetDisplayMinimapIcon(displayMinimapIcon)
-	TodoChecklisterSettingsDB.displayMinimapIcon = displayMinimapIcon
+---@param value boolean @Whether or not to display the icon on minimap
+function TCSettings:SetDisplayMinimapIcon(value)
+	TodoChecklisterSettingsDB.displayMinimapIcon = value
 end
 
 ---@return boolean @Whether or not to display chat messages
-function Settings:ChatMuted()
-	return TodoChecklisterSettingsDB.chatMuted
+function TCSettings:ChatShow()
+	return TodoChecklisterSettingsDB.chatShow
 end
 
----@param displayLinked boolean @Sets whether or not to display chat messages
-function Settings:SetChatMuted(chatMuted)
-	TodoChecklisterSettingsDB.chatMuted = chatMuted
+---@param value boolean @Sets whether or not to display chat messages
+function TCSettings:SetChatShow(value)
+	TodoChecklisterSettingsDB.chatShow = value
 end
 
 ---@return number @The alpha value of 0 to 1 for the window opacity when hovering the mouse
-function Settings:OpacityOnHover()
+function TCSettings:OpacityOnHover()
 	return TodoChecklisterSettingsDB.windowOpacityOnHover
 end
 
 ---@param opacity number @Sets the alpha value of 0 to 1 for the window opacity when hovering the mouse
-function Settings:SetOpacityOnHover(opacity)
+function TCSettings:SetOpacityOnHover(opacity)
 	TodoChecklisterSettingsDB.windowOpacityOnHover = opacity
 end
 
 ---@return boolean @Whether or not should count bank items on linked items
-function Settings:DisplayBankOnLinked()
+function TCSettings:DisplayBankOnLinked()
 	return TodoChecklisterSettingsDB.displayBankOnLinked
 end
 
----@param displayBankOnLinked boolean @Whether or not should count bank items on linked items
-function Settings:SetDisplayBankOnLinked(displayBankOnLinked)
-	TodoChecklisterSettingsDB.displayBankOnLinked = displayBankOnLinked
+---@param value boolean @Whether or not should count bank items on linked items
+function TCSettings:SetDisplayBankOnLinked(value)
+	TodoChecklisterSettingsDB.displayBankOnLinked = value
 end
 
 ---@return boolean @Whether or not should count charges on linked items
-function Settings:DisplayChargesOnLinked()
+function TCSettings:DisplayChargesOnLinked()
 	return TodoChecklisterSettingsDB.displayChargesOnLinked
 end
 
----@param displayChargesOnLinked boolean @Sets whether or not should count charges on linked items
-function Settings:SetDisplayChargesOnLinked(displayChargesOnLinked)
-	TodoChecklisterSettingsDB.displayChargesOnLinked = displayChargesOnLinked
+---@param value boolean @Sets whether or not should count charges on linked items
+function TCSettings:SetDisplayChargesOnLinked(value)
+	TodoChecklisterSettingsDB.displayChargesOnLinked = value
 end
 
 --------------------------------------
 -- Lifecycle Events
 --------------------------------------
----
----Resets all properties to their default values
-function Settings:Defaults()
-	TodoChecklisterSettingsDB =
-		TableUtils:Assign(
-		TodoChecklisterSettingsDB,
-		{
-			isShown = true,
-			keepFocus = false,
-			isKeepFocusShown = true,
-			windowOpacity = 1,
-			playFanfare = true,
-			displayLinked = true,
-			displayMinimapIcon = true,
-			chatMuted = false,
-			windowOpacityOnHover = 1,
-			displayBankOnLinked = true,
-			displayChargesOnLinked = true
-		}
-	)
+
+---Sets options to default values
+function SetDefaultTodoChecklisterSettings()
+	TodoChecklisterSettingsDB = {}
+	TodoChecklisterSettingsDB["isShown"] = true
+	TodoChecklisterSettingsDB["isKeepFocusShown"] = false
+	TodoChecklisterSettingsDB["keepFocus"] = false
+	TodoChecklisterSettingsDB["playFanfare"] = true
+	TodoChecklisterSettingsDB["displayLinked"] = false
+	TodoChecklisterSettingsDB["displayMinimapIcon"] = true
+	TodoChecklisterSettingsDB["chatShow"] = true
+	TodoChecklisterSettingsDB["displayBankOnLinked"] = false
+	TodoChecklisterSettingsDB["displayChargesOnLinked"] = false
+	TodoChecklisterSettingsDB["windowOpacity"] = 1
+	TodoChecklisterSettingsDB["windowOpacityOnHover"] = 1
 end
 
----
----Initializes required properties for this class
-function Settings:Init()
+---Initializes options
+function TCSettings:Init()
 	if (not TodoChecklisterSettingsDB) then
-		---
+		print("MAKING DEFAULTS")
 		---The SavedVariable where the settings are stored into
-		---@class TodoSettings
 		---@field public isShown boolean|nil @Whether or not the window is displayed on the user's screen
 		---@field public keepFocus boolean|nil @Whether or not the KeepFocus checkbox is checked
 		---@field public isKeepFocusShown boolean|nil @Whether or not the KeepFocus checkbox is displayed to the user
@@ -169,39 +155,14 @@ function Settings:Init()
 		---@field public playFanfare boolean|nil @Whether or not a fanfare sound should be played
 		---@field public displayLinked boolean|nil @Whether or not to display linked items count from bag
 		---@field public displayMinimapIcon boolean|nil @Whether or not to display the icon on minimap
-		---@field public chatMuted boolean|nil @Whether or not to display chat messages
+		---@field public chatShow boolean|nil @Whether or not to display chat messages
 		---@field public windowOpacityOnHover number|nil @The alpha value of 0 to 1 for the window opacity when hovering the mouse
 		---@field public displayBankOnLinked number|nil @Whether or not should count bank items on linked items
 		---@field public displayChargesOnLinked number|nil @Whether or not should count charges on linked items
-		TodoChecklisterSettingsDB = {}
-		self:Defaults()
+		SetDefaultTodoChecklisterSettings()
 	end
 
-	if (TodoChecklisterSettingsDB.isShown == nil) then
-		self:SetIsShown(true)
-	end
+	-- TodoAddon.TableUtils:printTable(TodoChecklisterSettingsDB)
+	-- print(TodoChecklisterSettingsDB.isShown)
 
-	if (TodoChecklisterSettingsDB.isKeepFocusShown == nil) then
-		self:SetIsKeepFocusShown(true)
-	end
-
-	if (TodoChecklisterSettingsDB.playFanfare == nil) then
-		self:SetPlayFanfare(true)
-	end
-
-	if (TodoChecklisterSettingsDB.displayLinked == nil) then
-		self:SetDisplayLinked(true)
-	end
-
-	if (TodoChecklisterSettingsDB.displayMinimapIcon == nil) then
-		self:SetDisplayMinimapIcon(true)
-	end
-
-	if (TodoChecklisterSettingsDB.displayChargesOnLinked == nil) then
-		self:SetDisplayBankOnLinked(true)
-	end
-
-	if (TodoChecklisterSettingsDB.displayChargesOnLinked == nil) then
-		self:SetDisplayChargesOnLinked(true)
-	end
 end
