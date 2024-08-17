2c6a0799ee5923cf51c8ec667b3434c8322d7b0c
hutcho <hutcho@users.noreply.github.com>
2024-08-17 23:33:42 +1000

    Set min frame size

--- src/components/ResponsiveFrame.lua
+++ src/components/ResponsiveFrame.lua
@@ -1,8 +1,4 @@
---------------------------------------
--- Imports
---------------------------------------
----@class TodoAddon
-local TodoAddon = select(2, ...)
+local _ , TodoAddon = ...
 
 --------------------------------------
 -- Declarations
@@ -15,47 +11,33 @@ local ResponsiveFrame = TodoAddon.ResponsiveFrame
 --------------------------------------
 -- ResponsiveFrame functions
 --------------------------------------
----
----Sets up required properties so the frame can properly function
+
+---Sets up required properties so the frame can resize
 function ResponsiveFrame:OnLoad(frame)
+
   self.frame = frame
+
+  local min_width = 200
+  local min_height = 200
+  frame:SetResizeBounds(min_width, min_height)
 
   frame:RegisterForDrag("LeftButton")
   frame:SetScale(1)
-  frame.x = frame:GetLeft()
-  frame.y = (frame:GetTop() - frame:GetHeight())
 
-  frame:SetScript(
-    "OnDragStart",
-    function(frame)
-      frame.isMoving = true
-      frame:StartMoving()
+  -- cartesian coords
+  frame.x_leftedge = frame:GetLeft()
+  frame.y_bottomedge = (frame:GetTop() - frame:GetHeight())
+
+  frame:SetScript("OnDragStart", function(self, button)
+      self:StartMoving()
     end
   )
 
-  frame:SetScript(
-    "OnDragStop",
-    function(frame)
-      frame.isMoving = false
-      frame:StopMovingOrSizing()
-      frame.x = frame:GetLeft()
-      frame.y = (frame:GetTop() - frame:GetHeight())
-      frame:ClearAllPoints()
-      frame:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", frame.x, frame.y)
+  frame:SetScript("OnDragStop", function(self, button)
+      self:StopMovingOrSizing()
     end
   )
 
-  frame:SetScript(
-    "OnUpdate",
-    function(frame)
-      if frame.isMoving == true then
-        frame.x = frame:GetLeft()
-        frame.y = (frame:GetTop() - frame:GetHeight())
-        frame:ClearAllPoints()
-        frame:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", frame.x, frame.y)
-      end
-    end
-  )
 end
 
 --------------------------------------
@@ -65,12 +47,25 @@ function OnLoad(frame)
   ResponsiveFrame:OnLoad(frame)
 end
 
+function OnMouseDown(self, button)
+  -- Fix a bug if you click too much in the scale button
+  -- self:GetParent():StartMoving()
+  -- self:GetParent():StopMovingOrSizing()
+
+  if button == "LeftButton" then
+    self.isSizing = true
+    self:GetParent():StartSizing("BOTTOMRIGHT")
+  elseif button == "RightButton" then
+    self.isScaling = true
+  end
+end
+
+
 function OnUpdate(self)
   if (self.isSizing == true) then
-    -- If the width or height is less than the allowed ammount
-    -- stop resizing
+    -- If the width or height is less than the allowed amount stop resizing
     local width, height = self:GetParent():GetWidth(), self:GetParent():GetHeight()
-    if (width <= 105 or height <= 140) then
+    if (width <= 199 or height <= 199) then
       self.isSizing = false
       self:GetParent():StopMovingOrSizing()
     end
@@ -100,36 +95,9 @@ end
 function OnMouseUp(self, button)
   if button == "LeftButton" then
     self.isSizing = false
-    self:GetParent():StopMovingOrSizing()
   elseif button == "RightButton" then
     self.isScaling = false
   end
-end
-
-function OnMouseDown(self, button)
-  local width, height = self:GetParent():GetWidth(), self:GetParent():GetHeight()
-
-  -- If the width or height is less than the allowed ammount
-  -- Resets the size and don't do anything
-  if (width <= 116) then
-    self:GetParent():SetWidth(120)
-  end
-
-  if (height <= 146) then
-    self:GetParent():SetHeight(147)
-  end
-
-  -- Fix a bug if you click too much in the scale button
-  self:GetParent():StartMoving()
   self:GetParent():StopMovingOrSizing()
 
-  if button == "LeftButton" then
-    self.isSizing = true
-    self:GetParent():StartSizing("BOTTOMRIGHT")
-    self:GetParent():SetUserPlaced(true)
-  end
-
-  if button == "RightButton" then
-    self.isScaling = true
-  end
 end
