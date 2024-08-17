13a1705995c6053e096ab5117ccd564a817c6c49
hutcho <hutcho@users.noreply.github.com>
2024-08-17 22:06:42 +1000

    Adding minimap code, not hiding on first display

--- src/modules/minimapIcon.lua
+++ src/modules/minimapIcon.lua
@@ -1,15 +1,14 @@
 --------------------------------------
 -- Imports
 --------------------------------------
----@class TodoAddon
-local TodoAddon = select(2, ...)
----@type string
-local addonName = select(1, ...)
+local addonName, TodoAddon = ...
 
 ---@class TodoChecklisterFrame
 local TodoChecklisterFrame = TodoAddon.TodoChecklisterFrame
 ---@class Settings
-local Settings = TodoAddon.Settings
+local TCSettings = TodoAddon.Settings
+---@class InterfaceOptions
+local InterfaceOptions = TodoAddon.InterfaceOptions
 
 --------------------------------------
 -- Declarations
@@ -17,53 +16,56 @@ local Settings = TodoAddon.Settings
 TodoAddon.MinimapIcon = {}
 
 ---@class MinimapIcon
-local MinimapIcon = TodoAddon.MinimapIcon
+local TCMinimapIcon = TodoAddon.MinimapIcon
 
 --------------------------------------
 -- Lifecycle Events
 --------------------------------------
 ---
 ---Load required configuration for this class
-function MinimapIcon:LoadCFG()
-	if LibStub("LibDBIcon-1.0", true) then
-		local icon = LibStub("LibDBIcon-1.0")
-		if (Settings:DisplayMinimapIcon()) then
-			icon:Show(addonName)
-		else
-			icon:Hide(addonName)
-		end
+function TCMinimapIcon:LoadCFG()
+	local icon = LibStub("LibDBIcon-1.0")
+	if (TCSettings:DisplayMinimapIcon()) then
+		icon:Show(addonName)
+		-- print("MinimapIcon:LoadCFG SHOWING ICON")
+	else
+		icon:Hide(addonName)
+		-- print("MinimapIcon:LoadCFG HIDING ICON")
 	end
+
 end
 
----
 ---Initializes the minimap icon if the user can have it
-function MinimapIcon:Init()
-	if type(TodoChecklisterMapIcon) ~= "table" then
-		---
+function TCMinimapIcon:Init()
+	if not type(TodoChecklisterMapIcon) == "table" then
 		---The SavedVariable where the map settings are stored into
 		---@class TodoMapIcon
-		TodoChecklisterMapIcon = {hide = false}
+		TodoChecklisterMapIcon = {
+			profile = {
+				minimap = {
+					hide = true,
+				},
+			},
+		}
 	end
 
-	if LibStub("LibDBIcon-1.0", true) then
-		local minimapIconLDB =
-			LibStub("LibDataBroker-1.1"):NewDataObject(
-			addonName .. "MinimapIcon",
-			{
-				type = "data source",
-				text = addonName,
-				icon = "Interface\\Icons\\INV_Misc_Note_03",
-				OnClick = function(self, button)
-					TodoChecklisterFrame:Toggle()
-				end,
-				OnTooltipShow = function(GameTooltip)
-					GameTooltip:SetText(addonName, 1, 1, 1)
-					GameTooltip:AddLine("Click to toggle your list", NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b)
-				end
-			}
-		)
+	local icon = LibStub("LibDBIcon-1.0")
 
-		LibStub("LibDBIcon-1.0"):Register(addonName, minimapIconLDB, TodoChecklisterMapIcon)
-		self:LoadCFG()
-	end
+	local minimapIconLDB = LibStub("LibDataBroker-1.1"):NewDataObject(addonName, {
+			type = "data source",
+			text = addonName,
+			icon = "Interface\\Icons\\INV_Misc_Note_03",
+			OnClick = function() TodoChecklisterFrame:Toggle() end,
+			OnTooltipShow = function(GameTooltip)
+				GameTooltip:SetText(addonName, 1, 1, 1)
+				GameTooltip:AddLine("Click to toggle your list", NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b)
+			end
+		}
+	)
+
+
+
+	icon:Register(addonName, minimapIconLDB, TodoChecklisterMapIcon)
+	InterfaceOptions:LoadCFG()
+	TCMinimapIcon:LoadCFG()
 end
